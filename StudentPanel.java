0fb5e525a4d644f0148a5ceaea2da3a668f828ad
Feek <fpm5022@gmail.com>
2015-09-15 17:16:34 -0400

    students imported from text file. hard paths for now

--- StudentPanel.java
+++ StudentPanel.java
@@ -1,6 +1,11 @@
 
 import java.awt.Color;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JPanel;
 
 /**
@@ -11,9 +16,13 @@ import javax.swing.JPanel;
 public class StudentPanel extends JPanel{
     private final Frame frame;
     private ArrayList<Student> students;
+    private final String studentFileLocation = "/Users/Feek/repos/412ProjectGrader/"; // TO DO: make this a setting
+    private final String studentFileName = "students.txt"; // to do: pull out like ^^^
+    private final String delimiter = ", |\\n"; // delmiter seperating students in students.txt. , and new line
     
     public StudentPanel(Frame frame) {
         this.frame = frame;
+        this.students = new ArrayList<>();
         importStudents();
         initComponents();
     }
@@ -25,6 +34,25 @@ public class StudentPanel extends JPanel{
     }
 
     private void importStudents() {
-        
+        try {
+            File file = new File(studentFileLocation + studentFileName);
+            Scanner read = new Scanner (file);
+            read.useDelimiter(delimiter);
+            
+            while(read.hasNext()) {
+                String name = read.next();
+                System.out.println(name);
+                String handle = read.next();
+                System.out.println(handle);
+                
+                students.add(new Student(name, handle));
+            }
+            
+            read.close();
+            
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(StudentPanel.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
