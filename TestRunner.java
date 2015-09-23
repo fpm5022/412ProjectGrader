23a6a7c72198d0c8b946f1c55efc5435e540291f
Feek <fpm5022@gmail.com>
2015-09-23 19:32:54 -0400

    command line args can now be passed in

--- TestRunner.java
+++ TestRunner.java
@@ -8,6 +8,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.lang.ProcessBuilder.Redirect;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -19,30 +21,48 @@ public class TestRunner {
     private final String classPath;
     private String mainClassName;
     private String[] scannerInput;
+    private String[] commandLineArgs;
     
-    public static void main(String[] args) {
-        String[] scannerInput = {"1", "1"};
-        TestRunner t = new TestRunner("/Users/Feek/Desktop/compiled/412/", "/Users/Feek/Desktop/compiled/412/smithjq/", "ArrayLoops", scannerInput);
-        t.runJava();
-    }
-    
-
     /*
-    path: parent directory of classpath
-    classpath: absolute path containing .class files
-    mainClassName: name of .class file to compile (not containing .class in name)
-    scannerInput: array of strings to pass as scanner input
+        example usage: 
+
+            String[] scannerInput = {"1", "1"};
+            String[] commandLineArgs = {};
+            TestRunner t = new TestRunner("/Users/Feek/Desktop/compiled/412/", "/Users/Feek/Desktop/compiled/412/smithjq/", "ArrayLoops", commandLineArgs, scannerInput);
+            t.runJava();
+
+        -------------
+
+        path: parent directory of classpath
+        classpath: absolute path containing .class files
+        mainClassName: name of .class file to compile (not containing .class in name)
+        commandLineArgs (OPTIONAL): array of strings to pass as command line args
+        scannerInput (OPTIONAL): array of strings to pass as scanner input 
     */
-    public TestRunner(String path, String classPath, String mainClassName, String[] scannerInput) {
+    public TestRunner(String path, String classPath, String mainClassName, String[] commandLineArgs, String[] scannerInput) {
         this.path = path;
         this.classPath = classPath;
         this.mainClassName = mainClassName;
+        this.commandLineArgs = commandLineArgs;
         this.scannerInput = scannerInput;
     }
 
     public void runJava() {
         try {
-            ProcessBuilder pb = new ProcessBuilder("java", mainClassName);
+            // this args list will contain all arguments to pass to the process builder
+            ArrayList<String> args = new ArrayList<>();
+            
+            args.add("java");
+            args.add(mainClassName);
+            
+            // if command line args were supplied, pop them into the list of args to pass 
+            // to the process builder
+            if(commandLineArgs != null) {
+                args.addAll(Arrays.asList(commandLineArgs));
+            }
+            
+            // will look something like `java ArrayLoops "1" "1"`
+            ProcessBuilder pb = new ProcessBuilder(args);
             
 //        Create environment map and set environmental variables
             Map<String, String> env = pb.environment();
@@ -56,33 +76,33 @@ public class TestRunner {
             pb.directory(nwd);
             
             pb.redirectErrorStream(true);
-            //pb.redirectOutput(Redirect.appendTo(outputFile));
+            //pb.redirectOutput(Redirect.appendTo(outputFile)); we want to be able to capture output in this class, so
+            // not writing to file direclty
             
             Process p = pb.start();
             
+            // these allow communication with program being tested
             InputStream stdout = p.getInputStream();
             OutputStream stdin = p.getOutputStream();
-            
             BufferedReader reader = new BufferedReader(new InputStreamReader(stdout));
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
             
-            // wait for the ready to be ready
+            // wait for the reader to be ready
             while(!reader.ready()) {
                 Thread.yield();
             }
             
             Scanner inScanner = new Scanner(stdout);
             
-            
             int i = 0; // index of scanner input to read from
-            System.out.println("scanner input length: " + scannerInput.length);
             
+            // read from program being tested
             while(inScanner.hasNextLine()) {
                 String line = inScanner.nextLine();
                 System.out.println(line);
                 
                 // only provide input if enough scanner inputs were provided 
-                if(scannerInput.length > i) {
+                if(scannerInput != null && scannerInput.length > i) {
                     System.out.println("writing: " + scannerInput[i]);
                     writer.write(scannerInput[i]);
                     writer.newLine();
