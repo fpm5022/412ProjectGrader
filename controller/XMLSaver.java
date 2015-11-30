4165ba0334c9af8a5b7e4a13060c2308652c7078
Feek <fpm5022@gmail.com>
2015-11-30 15:42:18 -0500

    xmlsaver docs

--- controller/XMLSaver.java
+++ controller/XMLSaver.java
@@ -8,21 +8,28 @@ import java.beans.XMLEncoder;
 import java.io.BufferedOutputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.XMLObject;
 
+/**
+ * This class saves the xmlobject when the program exits via a hook added in frame
+ */
 public class XMLSaver implements Runnable {
     private XMLEncoder encoder;
     private final String location; // location to save this xml file
-    private XMLObject object;
+    private final XMLObject object;
     
     public XMLSaver(String location, XMLObject xmlObject) {
         this.object = xmlObject;
         this.location = location;
     }
     
+    /**
+     * Given the supplied key, save the value into the xml object
+     * @param key
+     * @param value 
+     */
     public void addValueToWrite(String key, String value) {
         switch(key) {
             case "studentFileLocationAbsolutePath":
@@ -47,9 +54,7 @@ public class XMLSaver implements Runnable {
     public void run() {
         try {
             encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(location + "/paths.xml")));
-            
             encoder.writeObject(object);
-            
             encoder.close();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(XMLSaver.class.getName()).log(Level.SEVERE, null, ex);
