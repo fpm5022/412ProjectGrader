f7417e5b4ba37b439bfef9875ce05b338a6b196f
Feek <fpm5022@gmail.com>
2015-11-30 15:30:07 -0500

    cleanup file choosers

--- view/StudentFileChooser.java
+++ view/StudentFileChooser.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package view;
 
-import java.awt.Font;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
@@ -15,16 +9,12 @@ import javax.swing.filechooser.FileNameExtensionFilter;
  */
 public class StudentFileChooser extends JFileChooser {
     
-    
-
-    
     public StudentFileChooser() {
         this.setFileSelectionMode(JFileChooser.FILES_ONLY);
         this.setDialogType(JFileChooser.OPEN_DIALOG);
         FileNameExtensionFilter filter = new FileNameExtensionFilter("TEXT FILES", "txt", "text");
         this.setFileFilter(filter);
         this.setDialogTitle("Please select the text file containing students");
-
     }
     
     /**
