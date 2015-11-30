68c33accfc34e723bb3b0dedde432b9120fea5f0
Feek <fpm5022@gmail.com>
2015-11-30 15:37:00 -0500

    xmlobject docs

--- model/XMLObject.java
+++ model/XMLObject.java
@@ -4,6 +4,10 @@
 
 package model;
 
+/**
+ * 
+ * This struct is used for loading/saving the project settings
+ */
 public class XMLObject {
     public String studentFileLocationAbsolutePath;
     public String mainClassName;
