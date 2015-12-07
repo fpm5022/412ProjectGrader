91f9aa0885ccebd45c5e6e6ad911d0bdb3de01ed
fpm5022 <fpm5022@110-L09.up.ist.local>
2015-12-06 19:13:41 -0500

    Added Labels for the text fields and some positioning constants

--- view/FunctionsPanel.java
+++ view/FunctionsPanel.java
@@ -2,7 +2,6 @@ package view;
 
 import controller.BatchGUIController;
 import controller.FunctionsPanelController;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.beans.XMLDecoder;
 import javax.swing.JButton;
@@ -17,8 +16,6 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import javax.swing.JPanel;
-import javax.swing.border.Border;
-import javax.swing.border.LineBorder;
 
 public class FunctionsPanel extends JPanel {
     public final Frame frame;
@@ -29,15 +26,17 @@ public class FunctionsPanel extends JPanel {
     public XMLDecoder readPaths;
     public FunctionsPanelModel model;
     private final FunctionsPanel self;
+    private JLabel title, mainClassNameLabel;
     
-    private JLabel jLabel1;
+    private final int COMPONENT_LEFT = 10, COMPONENT_MIDDLE = 230, COMPONENT_RIGHT = 500, COMPONENT_HEIGHT = 30;
+    private JLabel commandLineArgsLabel;
+    private JLabel expectedResultsLabel;
 
     public FunctionsPanel(Frame frame, XMLObject xmlObject) {
         this.frame = frame;
         this.model = new FunctionsPanelModel();
         this.self = this;
         model.setDefaults(xmlObject);
-        
         initComponents();
         this.setLayout(null);
         this.setVisible(true);
@@ -51,20 +50,23 @@ public class FunctionsPanel extends JPanel {
         this.sourceDirectoryButton = BatchGUIController.generateButton("Source Directory");
         this.clearOutputButton = BatchGUIController.generateButton("Clear Output Area");
         this.sourceDirectoryTextField = new JTextField();
-        this.jLabel1 = new JLabel();
+        this.title = new JLabel();
         this.compileAndTestButton = BatchGUIController.generateButton("Compile And Test");
         this.cmdLnArg = new JTextField();
         this.expectedOutput = new JTextField();
         this.textPanel = new TextPanel();
         this.outputScroller = new JScrollPane(textPanel);
         this.mainClassNameTextField = new JTextField();
+        this.mainClassNameLabel = BatchGUIController.generateLabel("Main Class Name (include .java)");
+        this.commandLineArgsLabel = BatchGUIController.generateLabel("Command Line Args (CSV)");
+        this.expectedResultsLabel = BatchGUIController.generateLabel("Expected Output");
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24));
-        jLabel1.setText("Batch Tester");
-        jLabel1.setBounds(frame.WIDTH / 3 - 150, 20, 400, 40);
-        this.add(jLabel1);
+        title.setFont(new java.awt.Font("Tahoma", 0, 24));
+        title.setText("Batch Tester");
+        title.setBounds(frame.WIDTH / 3 - 150, 20, 400, 40);
+        this.add(title);
           
-        sourceDirectoryButton.setBounds(10, 100, 150, 30);
+        sourceDirectoryButton.setBounds(COMPONENT_LEFT, 100, 150, COMPONENT_HEIGHT);
         this.add(sourceDirectoryButton);
         sourceDirectoryButton.addActionListener(new java.awt.event.ActionListener() {
             @Override
@@ -74,12 +76,11 @@ public class FunctionsPanel extends JPanel {
         });
 
         sourceDirectoryTextField = new JTextField();
-        sourceDirectoryTextField.setBounds(230, 100, 400, 30);
-        sourceDirectoryTextField.setEditable(false);
+        sourceDirectoryTextField.setBounds(COMPONENT_MIDDLE, 100, 400, COMPONENT_HEIGHT);
         sourceDirectoryTextField.setText(model.sourceCodeDirectory);
         this.add(sourceDirectoryTextField);
 
-        compilePathButton.setBounds(10, 150, 150, 30);
+        compilePathButton.setBounds(COMPONENT_LEFT, 150, 150, COMPONENT_HEIGHT);
         this.add(compilePathButton);
         compilePathButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -88,20 +89,25 @@ public class FunctionsPanel extends JPanel {
         });
 
         compilePathTextField = new JTextField();
-        compilePathTextField.setBounds(230, 150, 400, 30);
-        compilePathTextField.setEditable(false);
+        compilePathTextField.setBounds(COMPONENT_MIDDLE, 150, 400, COMPONENT_HEIGHT);
         compilePathTextField.setText(model.compilePathDirectory);
         this.add(compilePathTextField);
 
+        commandLineArgsLabel.setBounds(COMPONENT_LEFT, 250, 300, COMPONENT_HEIGHT);
+        this.add(commandLineArgsLabel);
+        
         cmdLnArg.setText(model.commandLineArguments);
-        cmdLnArg.setBounds(10, 250, 300, 30);
+        cmdLnArg.setBounds(COMPONENT_MIDDLE, 250, 300, COMPONENT_HEIGHT);
         this.add(cmdLnArg);
         
+        expectedResultsLabel.setBounds(COMPONENT_LEFT, 300, 300, COMPONENT_HEIGHT);
+        this.add(expectedResultsLabel);
+        
         expectedOutput.setText(model.expectedTestOutput);
-        expectedOutput.setBounds(10, 300, 300, 30);
+        expectedOutput.setBounds(COMPONENT_MIDDLE, 300, 300, COMPONENT_HEIGHT);
         this.add(expectedOutput);
 
-        compileAndTestButton.setBounds(500, 365, 150, 30);
+        compileAndTestButton.setBounds(COMPONENT_RIGHT, 365, 150, COMPONENT_HEIGHT);
         this.add(compileAndTestButton);
         compileAndTestButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -110,14 +116,17 @@ public class FunctionsPanel extends JPanel {
         });
         
         outputScroller.setVisible(true);
-        outputScroller.setBounds(10,365,450, 300);
+        outputScroller.setBounds(COMPONENT_LEFT,365,450, 300);
         this.add(outputScroller);
         
+        mainClassNameLabel.setBounds(COMPONENT_LEFT, 200, 300, COMPONENT_HEIGHT);
+        this.add(mainClassNameLabel);
+        
         mainClassNameTextField.setText(model.mainClassName);
-        mainClassNameTextField.setBounds(230, 200, 400, 30);
+        mainClassNameTextField.setBounds(COMPONENT_MIDDLE, 200, 400, COMPONENT_HEIGHT);
         this.add(mainClassNameTextField);
         
-        clearOutputButton.setBounds(500, 405, 150, 30);
+        clearOutputButton.setBounds(COMPONENT_RIGHT, 405, 150, COMPONENT_HEIGHT);
         clearOutputButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 FunctionsPanelController.clearOutputArea(textPanel);
